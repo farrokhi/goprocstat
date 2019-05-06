24f076b38cfada6cba42ecd18a4fd2b48186adb8
Babak Farrokhi <babak@farrokhi.net>
2019-05-06 16:11:38 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module goprocstat
+
+require (
+	github.com/cockroachdb/apd v1.1.0
+	github.com/shirou/gopsutil v2.18.12+incompatible
+	golang.org/x/sys v0.0.0-20190506115046-ca7f33d4116e // indirect
+)
+
+go 1.12
---
+++ goprocstat.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/shirou/gopsutil/process"
+	"time"
+)
+
+func main() {
+
+	flagPID := flag.Int("PID", 1, "Gimme your PID")
+	flag.Parse()
+
+	p, _ := process.NewProcess(int32(*flagPID))
+	v, err := p.Times()
+
+	for {
+		if err == nil {
+			fmt.Printf("%d  pid: %d   user: %f\t system: %f\n", time.Now().Unix(), *flagPID, v.User, v.System)
+		} else {
+			fmt.Println(err)
+		}
+		time.Sleep(time.Second)
+	}
+
+}
