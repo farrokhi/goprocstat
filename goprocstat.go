c2ea7d14a0b7a3f3a9b6dd354d49b9c5e3cdf0ac
Babak Farrokhi <babak@farrokhi.net>
2019-05-07 14:49:20 +0200

    Add "-n" to limit number of stats before exit

--- goprocstat.go
+++ goprocstat.go
@@ -4,24 +4,31 @@ import (
 	"flag"
 	"fmt"
 	"github.com/shirou/gopsutil/process"
+	"os"
 	"time"
 )
 
 func main() {
 
-	flagPID := flag.Int("p", 1, "Gimme your PID")
+	flagPID := flag.Int("p", 1, "PID for process to monitor")
+	flagNUM := flag.Int("n", -1, "Stop after n times of displaying stat")
 	flag.Parse()
 
 	p, _ := process.NewProcess(int32(*flagPID))
 	v, err := p.Times()
+	cnt:=0
 
 	for {
+		if *flagNUM >= 0  &&  cnt >= *flagNUM {
+				os.Exit(0)
+			}
+
 		if err == nil {
 			fmt.Printf("%d  pid: %d   user: %f\t system: %f\n", time.Now().Unix(), *flagPID, v.User, v.System)
 		} else {
 			fmt.Println(err)
 		}
 		time.Sleep(time.Second)
+		cnt++
 	}
-
 }
